b7fd0b4f11e497b7b01d50cefc8475aed84a6b1f
Jonathan Ellis <jbellis@datastax.com>
2024-02-03 11:18:55 -0600

    old load -> compute-and-load new load -> load [reads embeddings from .json.gz]

--- load.py
+++ load.py
@@ -1,56 +1,48 @@
+import gzip
 import itertools
 import json
 import random
 import threading
 import time
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from tqdm.auto import tqdm
-
-from colbert.infra.config import ColBERTConfig
-from colbert.modeling.checkpoint import Checkpoint
-from colbert.indexing.collection_encoder import CollectionEncoder
 
 from cassandra.concurrent import execute_concurrent_with_args
 from db import DB
 
+#
+# Load .json.gz files into Cassandra
+#
 
 thread_local_storage = threading.local()
 def get_threadlocals():
     if not hasattr(thread_local_storage, 'db_handle'):
         thread_local_storage.db_handle = DB()
-
-        cf = ColBERTConfig(checkpoint='checkpoints/colbertv2.0')
-        cp = Checkpoint(cf.checkpoint, colbert_config=cf)
-        thread_local_storage.encoder = CollectionEncoder(cf, cp)
-    return thread_local_storage.db_handle, thread_local_storage.encoder
+    return thread_local_storage.db_handle
 
 
-def load_file(full_path):
-    db, encoder = get_threadlocals()
+def load_file(compressed_path):
+    db = get_threadlocals()
 
     # load json contents
-    with open(full_path, 'r') as f:
+    with gzip.open(compressed_path, 'rt', encoding='UTF-8') as f:
         doc = json.load(f)
     title = doc['title']
 
-    passages = []
     part = 0
+    n_embeddings = 0
     while str(part) in doc:
         content = doc[str(part)]['content']
         dpr = doc[str(part)]['embedding']
         db.session.execute(db.insert_chunk_stmt, (title, part, content, dpr))
-        passages.append(content)
-        part += 1
-    embeddings_flat, counts = encoder.encode_passages(passages)
-
-    # split up embeddings_flat by counts, a list of the number of tokens in each passage
-    start_indices = [0] + list(itertools.accumulate(counts[:-1]))
-    embeddings_by_part = [embeddings_flat[start:start+count] for start, count in zip(start_indices, counts)]
-    for part, embeddings in enumerate(embeddings_by_part):
+        embeddings = doc[str(part)]['colbert_embedding']
         execute_concurrent_with_args(db.session, db.insert_colbert_stmt, [(title, part, i, e) for i, e in enumerate(embeddings)])
-    print(f'Loaded {title} in {part} parts and {len(embeddings_flat)} embeddings')
+        part += 1
+        n_embeddings += len(embeddings)
+
+    print(f'Loaded {title} in {part} parts and {n_embeddings} embeddings')
 
 
 def main():
@@ -59,12 +51,21 @@ def main():
     time.sleep(1)
 
     print("Inserting data")
-    num_threads = 4
+    num_threads = 16
+    chunks_path = Path('/home/jonathan/datasets/wiki50k-chunks')
+    # for compressed_path in chunks_path.iterdir():
+    #     load_file(compressed_path)
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        chunks_path = Path('../wiki50k/chunks')
-        executor.map(load_file, chunks_path.iterdir())
-    # for path in Path('../wiki50k/chunks').iterdir():
-    #     load_file(path)
+        # Submit all tasks and hold their futures in a list
+        futures = [executor.submit(load_file, path) for path in chunks_path.iterdir()]
+
+        # Iterate over the futures as they complete (whether successfully or due to exceptions)
+        for future in as_completed(futures):
+            try:
+                # Accessing the result of the future will raise any exceptions caught during execution
+                future.result()
+            except Exception as exc:
+                print(f'An exception occurred: {exc}')
 
 if __name__ == '__main__':
     main()

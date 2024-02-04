35f7aff973c3e860eab1ab79d479e27b7dfb0e05
Jonathan Ellis <jbellis@datastax.com>
2024-02-04 12:13:27 -0600

    colbert25k keyspace

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, List
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 
+keyspace = "colbert25k"
 
 class DB:
     def __init__(self, **kwargs):
@@ -10,20 +11,20 @@ class DB:
         self.session.default_timeout = 60
 
         insert_chunk_cql = f"""
-        INSERT INTO colbert.chunks (title, part, body, ada002_embedding)
+        INSERT INTO {keyspace}.chunks (title, part, body, ada002_embedding)
         VALUES (?, ?, ?, ?)
         """
         self.insert_chunk_stmt = self.session.prepare(insert_chunk_cql)
 
         insert_colbert_cql = f"""
-        INSERT INTO colbert.colbert_embeddings (title, part, embedding_id, bert_embedding)
+        INSERT INTO {keyspace}.colbert_embeddings (title, part, embedding_id, bert_embedding)
         VALUES (?, ?, ?, ?)
         """
         self.insert_colbert_stmt = self.session.prepare(insert_colbert_cql)
 
         query_ada_cql = f"""
         SELECT title, body
-        FROM colbert.chunks
+        FROM {keyspace}.chunks
         ORDER BY ada002_embedding ANN OF ?
         LIMIT 5
         """
@@ -31,7 +32,7 @@ class DB:
 
         query_colbert_ann_cql = f"""
         SELECT title, part
-        FROM colbert.colbert_embeddings
+        FROM {keyspace}.colbert_embeddings
         ORDER BY bert_embedding ANN OF ?
         LIMIT 5
         """
@@ -39,14 +40,14 @@ class DB:
 
         query_colbert_parts_cql = f"""
         SELECT title, part, bert_embedding
-        FROM colbert.colbert_embeddings
+        FROM {keyspace}.colbert_embeddings
         WHERE title = ? AND part = ?
         """
         self.query_colbert_parts_stmt = self.session.prepare(query_colbert_parts_cql)
 
         query_part_by_pk = f"""
         SELECT body
-        FROM colbert.chunks
+        FROM {keyspace}.chunks
         WHERE title = ? AND part = ?
         """
         self.query_part_by_pk_stmt = self.session.prepare(query_part_by_pk)

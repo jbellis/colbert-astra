607e704a5fb33faa950ae591097741a97cd0ef18
Jonathan Ellis <jbellis@datastax.com>
2024-02-04 12:13:21 -0600

    comment

--- serve.py
+++ serve.py
@@ -42,6 +42,7 @@ def retrieve_colbert(query):
         rows = db.session.execute(db.query_colbert_parts_stmt, [title, part])
         embeddings_for_part = [tensor(row.bert_embedding) for row in rows]
         scores[(title, part)] = sum(maxsim(qv, embeddings_for_part) for qv in query_encodings)
+    # load the source chunk for the top 5
     docs_by_score = sorted(scores, key=scores.get, reverse=True)[:5]
     L = []
     for title, part in docs_by_score:
